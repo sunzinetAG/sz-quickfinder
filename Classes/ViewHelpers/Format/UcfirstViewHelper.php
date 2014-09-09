938752d5f297f76b9bea661790caac025ee92499
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2014-09-09 07:10:47 +0000

    refs #21421: Partials klein schreiben

---
+++ Classes/ViewHelpers/Format/UcfirstViewHelper.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Description of the class 'UcfirstViewHelper.php'
+ *
+ * @author Dennis Römmich <dennis@roemmich.eu>
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class Tx_SzIndexedSearch_ViewHelpers_Format_UcfirstViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @return string The result with first letter uppercase
+	 */
+	public function render() {
+		return ucfirst($this->renderChildren());
+	}
+
+}
+
+?>
