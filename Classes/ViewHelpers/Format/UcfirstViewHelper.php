9bba37ca347e5c6557c332413180e21617d54925
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2015-08-26 16:27:29 +0200

    Tests für den UcFirstViewHelper geschrieben

--- Classes/ViewHelpers/Format/UcfirstViewHelper.php
+++ Classes/ViewHelpers/Format/UcfirstViewHelper.php
@@ -1,19 +1,38 @@
 <?php
+namespace Sunzinet\SzIndexedSearch\ViewHelpers\Format;
+
 /**
- * Description of the class 'UcfirstViewHelper.php'
+ * Make a string's first character uppercase
  *
  * @author Dennis Römmich <dennis@roemmich.eu>
  * @copyright Copyright belongs to the respective authors
  * @license http://www.gnu.org/licenses/gpl.html
  * GNU General Public License, version 3 or later
  */
-class Tx_SzIndexedSearch_ViewHelpers_Format_UcfirstViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Class UcfirstViewHelper
+ *
+ * @package Sunzinet\ViewHelpers\Format
+ */
+class UcfirstViewHelper extends AbstractViewHelper {
 
 	/**
-	 * @return string The result with first letter uppercase
+	 * Make a string's first character uppercase
+	 *
+	 * @return string
 	 */
 	public function render() {
-		return ucfirst($this->renderChildren());
+		$string = $this->renderChildren();
+		if (!is_string($string)) {
+			throw new \InvalidArgumentException('Parameter $searchString must be of type string', 1440585046);
+		}
+
+		if ($string === '') {
+			throw new \InvalidArgumentException('Given String must not be Empty', 1440581637);
+		}
+		return ucfirst($string);
 	}
 
 }
