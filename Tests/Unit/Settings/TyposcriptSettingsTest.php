feab0ded106b6808e7ce574929033013aab00c53
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2017-12-21 15:54:08 +0100

    [TASK] Unittests for TyposcriptSettings

---
+++ Tests/Unit/Settings/TyposcriptSettingsTest.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace Sunzinet\SzQuickfinder\Tests\Settings;
+
+/**
+ * Class TyposcriptSettingsTest
+ */
+class TyposcriptSettingsTest extends \TYPO3\CMS\Core\Tests\UnitTestCase
+{
+    /**
+     * @var \Sunzinet\SzQuickfinder\TyposcriptSettings $subject
+     */
+    protected $subject;
+
+    public function setUp()
+    {
+        $this->subject = new \Sunzinet\SzQuickfinder\Settings\TyposcriptSettings([]);
+    }
+
+    /**
+     * @test
+     */
+    public function setUnavailablePropertyThrowsException()
+    {
+        $this->setExpectedException(\TYPO3\CMS\Extbase\Property\Exception\InvalidPropertyException::class);
+        $this->subject->setProperty('thisPropertyDoesNotExist', 'foobar');
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyClass()
+    {
+        $this->subject->setProperty('class', 'FooBar');
+        $this->assertTrue(($this->subject->getClass() === 'FooBar'));
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyRegEx()
+    {
+        $this->subject->setProperty('regEx', '%|%');
+        $this->assertTrue(($this->subject->getRegEx() === '%|%'));
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyMaxResultsWithType()
+    {
+        $this->subject->setProperty('maxResults', '7');
+        $this->assertSame(7, $this->subject->getMaxResults());
+        $this->subject->setProperty('maxResults', 7);
+        $this->assertSame(7, $this->subject->getMaxResults());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyIncludeNavHiddenPages()
+    {
+        $this->subject->setProperty('includeNavHiddenPages', '1');
+        $this->assertSame(true, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', 1);
+        $this->assertSame(true, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', 'true');
+        $this->assertSame(true, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', true);
+        $this->assertSame(true, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', '0');
+        $this->assertSame(false, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', 0);
+        $this->assertSame(false, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', 'false');
+        $this->assertSame(false, $this->subject->getIncludeNavHiddenPages());
+
+        $this->subject->setProperty('includeNavHiddenPages', false);
+        $this->assertSame(false, $this->subject->getIncludeNavHiddenPages());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertySearchFields()
+    {
+        $this->subject->setProperty('searchFields', 'foo,bar, foobar');
+        $this->assertSame(['foo', 'bar', 'foobar'], $this->subject->getSearchfields());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyAllowedFieldnames()
+    {
+        $this->subject->setProperty('allowedFieldnames', 'foo,bar, foobar');
+        $this->assertSame(['foo', 'bar', 'foobar'], $this->subject->getAllowedFieldnames());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertySearchString()
+    {
+        $this->mockGlobalDatabase();
+
+        $this->subject->setProperty('searchString', 'foobar');
+        $this->assertSame('foobar', (string)$this->subject->getSearchString());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyOrderBy()
+    {
+        $this->subject->setProperty('orderBy', 'uid');
+        $this->assertSame('uid', $this->subject->getOrderBy());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyAscending()
+    {
+        $this->subject->setProperty('ascending', '1');
+        $this->assertSame(true, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', 1);
+        $this->assertSame(true, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', 'true');
+        $this->assertSame(true, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', true);
+        $this->assertSame(true, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', '0');
+        $this->assertSame(false, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', 0);
+        $this->assertSame(false, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', 'false');
+        $this->assertSame(false, $this->subject->getAscending());
+
+        $this->subject->setProperty('ascending', false);
+        $this->assertSame(false, $this->subject->getAscending());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyScript()
+    {
+        $this->subject->setProperty('script', '/path/to/script');
+        $this->assertSame('/path/to/script', $this->subject->getScript());
+    }
+
+    /**
+     * @test
+     */
+    public function setPropertyParams()
+    {
+        $this->subject->setProperty('params', ['foo', 'bar']);
+        $this->assertSame(['foo', 'bar'], $this->subject->getParams());
+    }
+
+    /**
+     * @test
+     */
+    public function propertyWillBeSetIfPropertyExists()
+    {
+        $this->subject->setProperty('class', 'FooBar');
+        $this->assertSame('FooBar', $this->subject->getClass());
+    }
+
+    /**
+     * @test
+     */
+    public function constructorArgumentsWillBeConvertedToProperties()
+    {
+        $this->mockGlobalDatabase();
+
+        $subject = new \Sunzinet\SzQuickfinder\Settings\TyposcriptSettings([
+            'class' => 'Foo\bar',
+            'regEx' => '%|%',
+            'maxResults' => '7',
+            'includeNavHiddenPages' => true,
+            'searchFields' => 'foo, bar,foobar',
+            'allowedFieldnames' => 'foobar',
+            'searchString' => 'Lorem',
+            'orderBy' => 'foobar',
+            'ascending' => 'true',
+            'script' => '/foo/bar',
+            'params' => 'foo, bar',
+        ]);
+
+        $this->assertSame('Foo\bar', $subject->getClass());
+        $this->assertSame('%|%', $subject->getRegEx());
+        $this->assertSame(7, $subject->getMaxResults());
+        $this->assertSame(true, $subject->getIncludeNavHiddenPages());
+        $this->assertSame(['foo', 'bar', 'foobar'], $subject->getSearchFields());
+        $this->assertSame(['foobar'], $subject->getAllowedFieldnames());
+
+        $this->assertSame('foobar', (string)$subject->getSearchString());
+
+        $this->assertSame('foobar', $subject->getOrderBy());
+        $this->assertSame(true, $subject->getAscending());
+        $this->assertSame('/foo/bar', $subject->getScript());
+        $this->assertSame(['foo', 'bar'], $subject->getParams());
+    }
+
+    /**
+     * @return void
+     */
+    protected function mockGlobalDatabase()
+    {
+        $GLOBALS['TYPO3_DB'] = $this->getMock(\TYPO3\CMS\Core\Database\DatabaseConnection::class);
+        $GLOBALS['TYPO3_DB']->method('escapeStrForLike')->willReturn('foobar');
+        $GLOBALS['TYPO3_DB']->method('quoteStr')->willReturn('foobar');
+    }
+}
