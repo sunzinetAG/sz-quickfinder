aaf4aa6ec4a4f323c1b9ca16bee327983103f678
Dennis Römmich <Dennis.Roemmich@sunzinet.com>
2017-12-22 12:43:35 +0100

    [TASK] Functional tests for SearchController

---
+++ Tests/Functional/Controller/SearchControllerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Sunzinet\SzQuickfinder\Tests\Controller;
+
+/**
+ * Class SearchControllerTest
+ *
+ * @package Sunzinet\SzQuickfinder\Tests\Controller
+ */
+class SearchControllerTest extends \TYPO3\CMS\Core\Tests\FunctionalTestCase
+{
+    /**
+     * @var array
+     */
+    protected $testExtensionsToLoad = [
+        'typo3conf/ext/sz_quickfinder',
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->importDataSet('ntf://Database/pages.xml');
+        $this->setUpFrontendRootPage(1, [
+            'EXT:sz_quickfinder/Tests/Functional/Fixtures/page.ts',
+            'EXT:sz_quickfinder/Configuration/TypoScript/setup.txt',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function frontendRequest()
+    {
+        $response = $this->getFrontendResponse(1);
+        $this->assertSame('success', $response->getStatus());
+    }
+
+    /**
+     * @test
+     */
+    public function formIsVisibleIfTypoScriptIsIncluded()
+    {
+        $response = $this->getFrontendResponse(1);
+        $this->assertContains('<form action="" method="post">', $response->getContent());
+        $this->assertContains('<button type="submit">Suchen</button>', $response->getContent());
+        $this->assertContains('<div class="tx-quickfinder-searchbox-results"></div>', $response->getContent());
+    }
+}
---
+++ Tests/Functional/Fixtures/page.ts
@@ -0,0 +1,14 @@
+config {
+    no_cache = 1
+    debug = 0
+    xhtml_cleaning = 0
+    admPanel = 0
+    disableAllHeaderCode = 1
+    sendCacheHeaders = 0
+}
+
+page = PAGE
+page {
+    typeNum = 0
+    10 =< lib.tx_szquickfinder
+}
