7fbfd7c89d29066e32d3116a48e8e97930e08385
Christian Efthimoglou <christian.efthimoglou@sunzinet.com>
2022-04-10 11:28:43 +0200

    [CLEANUP] Adjust config files
    Related:

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,24 @@
 module.exports = function (grunt) {
-
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                compress: {
-                    drop_console: true
-                },
-                banner: '/*! \n' +
-                '* <%= pkg.name %> - v<%= pkg.version %> (<%= pkg.repository.url %>)\n' +
-                '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.company %>) <%= pkg.author.mail %>\n' +
-                '* Licensed under the <%= pkg.license %> license\n' +
-                '*/\n'
-            },
-            szFlyout: {
-                files: {
-                    'Resources/Public/JavaScript/SzQuickfinder.min.js': ['Resources/Public/JavaScript/SzQuickfinder.js']
-                }
-            }
-        }
-    });
-
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
-    grunt.registerTask('default', ['uglify']);
-
-};
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        compress: {
+          drop_console: true,
+        },
+        banner: '/*! \n' +
+          '* <%= pkg.name %> - v<%= pkg.version %> (<%= pkg.repository.url %>)\n' +
+          '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> (<%= pkg.company %>) <%= pkg.author.mail %>\n' +
+          '* Licensed under the <%= pkg.license %> license\n' +
+          '*/\n',
+      },
+      szFlyout: {
+        files: {
+          'Resources/Public/JavaScript/SzQuickfinder.min.js': ['Resources/Public/JavaScript/SzQuickfinder.js'],
+        },
+      },
+    },
+  })
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.registerTask('default', ['uglify'])
+}
