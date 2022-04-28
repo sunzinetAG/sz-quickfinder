f88471d16db2c1383cd5a0ccc1f075beea100612
Christian Efthimoglou <christian.efthimoglou@sunzinet.com>
2022-04-28 15:59:05 +0200

    [TASK] Deprecate UcfirstViewHelper
    Related:

--- Classes/ViewHelpers/Format/UcfirstViewHelper.php
+++ Classes/ViewHelpers/Format/UcfirstViewHelper.php
@@ -9,6 +9,9 @@ use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
 
+/**
+ * @deprecated
+ */
 final class UcfirstViewHelper extends AbstractViewHelper
 {
     use CompileWithContentArgumentAndRenderStatic;
@@ -26,12 +29,17 @@ final class UcfirstViewHelper extends AbstractViewHelper
      * @param Closure $renderChildrenClosure
      * @param RenderingContextInterface $renderingContext
      * @return string
+     * @deprecated
      */
     public static function renderStatic(
         array $arguments,
         Closure $renderChildrenClosure,
         RenderingContextInterface $renderingContext
     ): string {
+        trigger_error(
+            'ViewHelper "Ucfirst" from EXT:sz_quickfinder is deprecated since version 6.0.0-dev, will be removed in version 6.0.0. Use f:format.case() instead.',
+            E_USER_DEPRECATED
+        );
         return ucfirst($renderChildrenClosure());
     }
 }
